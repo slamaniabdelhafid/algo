08621349d535c9b6fa40cdef517cb8eeecdf0daf
slamaniabdelhafid <st130302@student.spbu.ru>
2025-04-17 17:44:13 +0300

    Update ascii85.h

--- src/ascii85.h
+++ src/ascii85.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <vector>
 #include <string>
-#include <cstdint>
+#include <cstdint> 
 
 std::string encode(const std::vector<uint8_t>& data);
 std::vector<uint8_t> decode(const std::string& encoded);
