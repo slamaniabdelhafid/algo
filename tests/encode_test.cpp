f8ffcee0935130e05875c9b63011ede7c22744df
slamaniabdelhafid <st130302@student.spbu.ru>
2025-04-24 13:35:12 +0300

    Update encode_test.cpp

--- tests/encode_test.cpp
+++ tests/encode_test.cpp
@@ -1,39 +1,25 @@
 #include <gtest/gtest.h>
-#include "../src/ascii85.h" 
-#include <cstdint>         
-#include <stdexcept>        
+#include "../src/ascii85.h"
+#include <cstdint>
+#include <stdexcept>
 
 TEST(EncodeTest, ZeroBlock) {
-    std::vector<uint8_t> data{0, 0, 0, 0};
+    std::string input(4, '\0');
     std::string expected = "z";
-    auto result = encode(data);
-    EXPECT_EQ(result, expected);
+    std::string result = ascii85::encode_ascii85(input);
+    ASSERT_EQ(result, expected);
 }
 
 TEST(EncodeTest, PartialBlock) {
-    std::vector<uint8_t> data{0x01, 0x02};
-    std::string expected = "!@#";
-    auto result = encode(data);
-    EXPECT_EQ(result, expected);
+    std::string input = "a";
+    std::string expected = "<~!1~>";
+    std::string result = ascii85::encode_ascii85(input);
+    ASSERT_EQ(result, expected);
 }
 
 TEST(EncodeTest, MixedData) {
-    std::vector<uint8_t> data{0xFF, 0x00, 0x7F, 0x80};
-    std::string expected = "~uZV";
-    auto result = encode(data);
-    EXPECT_EQ(result, expected);
-}
-
-TEST(EncodeTest, PartialBlockWithPadding) {
-    std::vector<uint8_t> data{0x01, 0x02};
-    std::string expected = "<~!@#~>";
-    auto result = encode(data);
-    EXPECT_EQ(result, expected);
-}
-
-TEST(DecodeTest, PartialBlockWithPadding) {
-    std::string input = "<~!@#~>";
-    std::vector<uint8_t> expected{0x01, 0x02};
-    auto result = decode(input);
-    EXPECT_EQ(result, expected);
+    std::string input = "test";
+    std::string expected = "9jqoC"; 
+    std::string result = ascii85::encode_ascii85(input);
+    ASSERT_EQ(result, expected);
 }
