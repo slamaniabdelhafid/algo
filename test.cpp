32564bba402cbe09743234015c60a337b105cbb4
slamaniabdelhafid <st130302@student.spbu.ru>
2025-05-09 14:59:30 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -4,17 +4,15 @@
 TEST(Ascii85Test, Encode) {
     EXPECT_EQ(ascii85::encode("hello"), "<~BOu!rDZ~>");
     EXPECT_EQ(ascii85::encode("Man"), "<~9jqo~>");
-    EXPECT_EQ(ascii85::encode(""), "<~~>");
 }
 
 TEST(Ascii85Test, EncodeNullBytes) {
-    std::string null_bytes(4, '\0'); 
+    std::string null_bytes(4, '\0');
     EXPECT_EQ(ascii85::encode(null_bytes), "<~z~>");
 }
 
 TEST(Ascii85Test, Decode) {
     EXPECT_EQ(ascii85::decode("<~BOu!rDZ~>"), "hello");
-    EXPECT_EQ(ascii85::decode("9jqo"), "Man");
-    EXPECT_EQ(ascii85::decode("<~~>"), "");
+    EXPECT_EQ(ascii85::decode("9jqo"), "Man");  
     EXPECT_EQ(ascii85::decode("z"), std::string(4, '\0'));
 }
