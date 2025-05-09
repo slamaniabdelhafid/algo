efe19be063d07431ad01b579dc1fe34de6252d90
slamaniabdelhafid <st130302@student.spbu.ru>
2025-05-09 13:20:50 +0300

    Delete ascii85.h

--- ascii85.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-
-namespace ascii85 {
-    std::string encode_ascii85(const std::string& input);
-    std::string decode_ascii85_to_string(const std::string& input);
-}
