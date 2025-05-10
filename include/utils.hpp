e4ec26d5421e4d13d65f7e484f2473cc0fb49c60
slamaniabdelhafid <st130302@student.spbu.ru>
2025-05-10 20:25:22 +0300

    Update utils.hpp

--- include/utils.hpp
+++ include/utils.hpp
@@ -1,17 +1,10 @@
 #pragma once
 #include <map>
 #include <string>
-#include <cstdint>
 #include <fstream>
-#include <nlohmann/json.hpp>
+#include "nlohmann/json.hpp"
 
 using json = nlohmann::json;
 
-void save_dictionary(
-    const std::map<uint8_t, std::string>& codes,
-    const std::string& file_path
-);
-
-std::map<uint8_t, std::string> load_dictionary(
-    const std::string& file_path
-);
+void save_dictionary(const std::map<uint8_t, std::string>& codes, const std::string& file_path);
+std::map<uint8_t, std::string> load_dictionary(const std::string& file_path);
