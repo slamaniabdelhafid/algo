09e4b0dec09a966b261d3175c970893def8f226c
slamaniabdelhafid <st130302@student.spbu.ru>
2025-05-10 20:34:58 +0300

    Update utils.hpp

--- include/utils.hpp
+++ include/utils.hpp
@@ -4,10 +4,10 @@
 #include <fstream>
 #include <cstdint>
 
-// Use relative path to json.hpp
-#include "./nlohmann/json.hpp"
-
-using json = nlohmann::json;
+// Define a simple JSON replacement if we can't use nlohmann
+namespace nlohmann {
+    using json = std::map<std::string, std::string>;
+}
 
 void save_dictionary(const std::map<uint8_t, std::string>& codes, const std::string& file_path);
 std::map<uint8_t, std::string> load_dictionary(const std::string& file_path);
