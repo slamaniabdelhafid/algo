b5b03d651fd08c4c185252bc09e141d71df8218d
slamaniabdelhafid <st130302@student.spbu.ru>
2025-05-10 20:11:10 +0300

    Create utils.cpp

---
+++ src/utils.cpp
@@ -0,0 +1,29 @@
+#include "utils.hpp"
+
+void save_dictionary(
+    const std::map<uint8_t, std::string>& codes,
+    const std::string& file_path
+) {
+    json j;
+    for (const auto& [sym, code] : codes) {
+        j[std::to_string(sym)] = code;
+    }
+    
+    std::ofstream file(file_path);
+    file << j.dump(4);
+}
+
+std::map<uint8_t, std::string> load_dictionary(
+    const std::string& file_path
+) {
+    std::ifstream file(file_path);
+    json j;
+    file >> j;
+    
+    std::map<uint8_t, std::string> codes;
+    for (auto& [sym_str, code] : j.items()) {
+        codes[static_cast<uint8_t>(std::stoi(sym_str))] = code;
+    }
+    
+    return codes;
+}
