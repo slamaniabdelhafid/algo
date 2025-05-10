759349d4bd44520f43bf72aac40e5b75bc83c5a3
slamaniabdelhafid <st130302@student.spbu.ru>
2025-05-10 20:09:03 +0300

    Create encoder.hpp

---
+++ include/encoder.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+#include <map>
+#include <vector>
+
+void encode_file(
+    const std::string& input_path,
+    const std::string& output_path,
+    const std::string& dict_path
+);
