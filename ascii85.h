3fa2788cb09190586bf7c2a8d6937e34ff37fd48
slamaniabdelhafid <st130302@student.spbu.ru>
2025-05-10 20:02:59 +0300

    Delete ascii85.h

--- ascii85.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-#include <stdexcept>  // Add this for std::runtime_error
-
-namespace ascii85 {
-    std::string encode(const std::string& data);
-    std::string decode(const std::string& data);
-}
