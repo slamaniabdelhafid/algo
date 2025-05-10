cbe6af3f6ea21d2274d1d9da5e9735deb4a34998
slamaniabdelhafid <st130302@student.spbu.ru>
2025-05-10 20:41:15 +0300

    Update decoder.cpp

--- src/decoder.cpp
+++ src/decoder.cpp
@@ -35,34 +35,6 @@ void decode_file(
     
     for (char bit : bit_string) {
         current_code += bit;
-        if (reverse_codes.find(current_code) != reverse_codes.end()) {
-            output.push_back(reverse_codes[current_code]);
-            current_code.clear();
-        }
-    }
-    
-    std::ofstream out(output_path, std::ios::binary);
-    out.write(reinterpret_cast<const char*>(output.data()), output.size());
-}
-
-
-void decode_file(
-    const std::string& compressed_path,
-    const std::string& dict_path,
-    const std::string& output_path
-) {
-    auto codes = load_dictionary(dict_path);
-    std::unordered_map<std::string, uint8_t> reverse_codes;
-    for (const auto& [sym, code] : codes) {
-        reverse_codes[code] = sym;
-    }
-    
-    std::string bit_string = read_compressed(compressed_path);
-    std::vector<uint8_t> output;
-    std::string current_code;
-    
-    for (char bit : bit_string) {
-        current_code += bit;
         if (reverse_codes.count(current_code)) {
             output.push_back(reverse_codes[current_code]);
             current_code.clear();
